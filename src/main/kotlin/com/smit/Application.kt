262fdab5051a2890ebeb5a386904985b1a856284
smit0101 <smitkalkani9099@gmail.com>
2022-05-14 19:12:59 +0530

    CORS ADDED

--- src/main/kotlin/com/smit/Application.kt
+++ src/main/kotlin/com/smit/Application.kt
@@ -2,12 +2,20 @@ package com.smit
 
 import com.smit.plugins.configureRouting
 import com.smit.plugins.configureSerialization
+import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import io.ktor.server.plugins.cors.routing.*
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "127.0.0.1") {
         configureRouting()
         configureSerialization()
+        install(CORS){
+            anyHost()
+        }
     }.start(wait = true)
+
 }
+
+
